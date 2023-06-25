d9966359471767aa4aaf6c0615dbb754647c6f5f
swagasoft <swagasoft@gmail.com>
2023-06-25 12:17:09 +0100

    added cubic time

---
+++ cubic_time.py
@@ -0,0 +1,18 @@
+
+def cubic_time(arr):  #N * N * N = N^3.
+    n = len(arr)
+    growth = 0
+    
+    for i in range(n): #N
+        for j in range(n):#N
+            for k in range(n):#N
+                growth += 1
+    print("Growth ", growth)
+    
+    
+# arr = [3,6,8,2,1,2,3,3]
+# arr = [3,6,8,2]
+arr = [3,6]
+cubic_time(arr)
+
+
